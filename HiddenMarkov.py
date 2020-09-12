dd163d9e83e17110f900a80897e9696602664b29
Xu Shen <shenxu2011@gmail.com>
2020-09-12 10:18:25 -0400

    logisticregression

---
+++ HiddenMarkov.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# An Implementation of Hidden Markov Model
+# Author: Xu Shen
+# =============================================================================
+
+import numpy as np
+
+class HiddenMarkov(object):
+    def forward(Q, V, A, B, O, PI):
+        '''
+        Forward pass
+        '''
+        pass
+    def backward():
+        pass
+    def viterbi():
+        pass
